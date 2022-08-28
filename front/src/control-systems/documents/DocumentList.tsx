9964f0e4bb9e86f7c2f3cc707b24d0fbf275c3a0
Andrei <blackmorse@live.com>
2022-08-29 00:28:11 +0400

    Added sorting by engine, and index numbers at table

--- front/src/control-systems/documents/DocumentList.tsx
+++ front/src/control-systems/documents/DocumentList.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import {TypedUseSelectorHook, useSelector} from 'react-redux'
 import {LoginState} from '../login/Login'
 import Document from '../models/Document'
@@ -16,36 +17,52 @@ const DocumentList = (props: Props) =>{
         let stateMapFunc = (state: LoginState) => state?.login
 
         let token = useAppSelector(stateMapFunc)?.token
-        
-        let documents: Array<Document> = props.documents as Array<Document>
+        let documents = props.documents 
+
+        type Sorting = 'engine' | undefined
+        let [ sortingField, setSortingFieldFunc ] = useState(undefined as Sorting)
+
+        function sortDocumentsByEngineNumber() {
+            setSortingFieldFunc('engine')
+        }
+
+        let sortedDocuments: Array<Document> 
+        if (sortingField === 'engine') {
+            sortedDocuments = documents.sort((doc1, doc2) => doc1.fileNameParameters.engineNumber - doc2.fileNameParameters.engineNumber)
+        } else {
+            sortedDocuments = documents
+        }
+
         return <table className="green_table">
         <thead>
           <tr>
-            <th>Тип двигателя</th>
-            <th>Число цилиндров</th>
-            <th>№ двигателя</th>
-            <th>Название объекта</th>
-            <th>№ двигателя на объекте</th>
-            <th>Язык</th>
-            <th>Дата</th>
-            <th>Ревизия</th>
-            <th>К сравнению</th>
+            <th></th>
+            <th className='text-center'>Тип двигателя</th>
+            <th className='text-center'>Число цилиндров</th>
+            <th className='text-center'><a className='link-light' href='#' onClick={_e => sortDocumentsByEngineNumber()}>№ двигателя</a></th>
+            <th className='text-center'>Название объекта</th>
+            <th className='text-center'>№ двигателя на объекте</th>
+            <th className='text-center'>Язык</th>
+            <th className='text-center'>Дата</th>
+            <th className='text-center'>Ревизия</th>
+            <th className='text-center'>К сравнению</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
-          {documents.map(document => {
+          {sortedDocuments.map((document, index) => {
 	  var cylindersArr = document.parameters.filter(parameter => parameter[0].code === 10003)[0]
           var cylinders = (cylindersArr !== undefined && cylindersArr.length > 1) ? cylindersArr[1] : ''
           return <tr key={"document_" + document.id}>
-            <td>{document.parameters.filter(parameter => parameter[0].code===10001)[0][1]}</td>
-            <td>{cylinders}</td>
-            <td>{document.fileNameParameters.engineNumber}</td>
-            <td>{document.fileNameParameters.objectName}</td>
-            <td>{document.fileNameParameters.objectEngineNumber}</td>
-            <td>{document.fileNameParameters.lang}</td>
-            <td>{document.fileNameParameters.date}</td>
-            <td>{document.fileNameParameters.revision}</td>
+            <td>{index + 1}</td>
+            <td className='text-center'>{document.parameters.filter(parameter => parameter[0].code===10001)[0][1]}</td>
+            <td className='text-center'>{cylinders}</td>
+            <td className='text-center'>{document.fileNameParameters.engineNumber}</td>
+            <td className='text-center'>{document.fileNameParameters.objectName}</td>
+            <td className='text-center'>{document.fileNameParameters.objectEngineNumber}</td>
+            <td className='text-center'>{document.fileNameParameters.lang}</td>
+            <td className='text-center'>{document.fileNameParameters.date}</td>
+            <td className='text-center'>{document.fileNameParameters.revision}</td>
             
             <td><input type="checkbox"
                       defaultChecked={props.checkedIds.has(document.id)} 
